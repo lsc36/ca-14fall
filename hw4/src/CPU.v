e67487abd35a9930ad744ae8ee34b7c013eae297
Shao-Chuan Lee <lsc36x@gmail.com>
2014-11-16 01:10:07 +0800

    connect wires (PC-IM-Reg)

--- hw4/src/CPU.v
+++ hw4/src/CPU.v
@@ -10,6 +10,9 @@ input               clk_i;
 input               rst_i;
 input               start_i;
 
+wire    [31:0]      pc_i, pc_o;
+wire    [31:0]      instr_o;
+
 /*
 Control Control(
     .Op_i       (),
@@ -29,23 +32,23 @@ Adder Add_PC(
 */
 
 PC PC(
-    .clk_i      (),
-    .rst_i      (),
-    .start_i    (),
-    .pc_i       (),
-    .pc_o       ()
+    .clk_i      (clk_i),
+    .rst_i      (rst_i),
+    .start_i    (start_i),
+    .pc_i       (pc_i),
+    .pc_o       (pc_o)
 );
 
 Instruction_Memory Instruction_Memory(
-    .addr_i     (),
-    .instr_o    ()
+    .addr_i     (pc_o),
+    .instr_o    (instr_o)
 );
 
 Registers Registers(
-    .clk_i      (),
-    .RSaddr_i   (),
-    .RTaddr_i   (),
-    .RDaddr_i   (),
+    .clk_i      (clk_i),
+    .RSaddr_i   (instr_o[25:21]),
+    .RTaddr_i   (instr_o[20:16]),
+    .RDaddr_i   (instr_o[15:11]),
     .RDdata_i   (),
     .RegWrite_i (),
     .RSdata_o   (),

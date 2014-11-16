d8cfee4ae3b1ad0ee4091382ff6bbface2264281
Shao-Chuan Lee <lsc36x@gmail.com>
2014-11-16 14:25:36 +0800

    implement ALU functions

--- hw4/src/ALU.v
+++ hw4/src/ALU.v
@@ -16,7 +16,13 @@ reg     [31:0]  data_o;
 reg             Zero_o;
 
 always@(data1_i or data2_i or ALUCtrl_i) begin
-    data_o = data1_i + data2_i;
+    case(ALUCtrl_i)
+        3'b000: data_o = data1_i & data2_i;  // and
+        3'b001: data_o = data1_i | data2_i;  // or
+        3'b010: data_o = data1_i + data2_i;  // add
+        3'b011: data_o = data1_i - data2_i;  // sub
+        3'b100: data_o = data1_i * data2_i;  // mul
+    endcase
     Zero_o = data_o == 0 ? 1'b1 : 1'b0;
 end
 

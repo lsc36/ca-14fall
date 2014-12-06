5c1eaa5a0d24489a1ae76eaddbe9465648394f07
Shao-Chuan Lee <lsc36x@gmail.com>
2014-12-07 00:33:19 +0800

    fix HazardDetection bubble condition

--- proj1/src/CPU.v
+++ proj1/src/CPU.v
@@ -109,12 +109,11 @@ ALU_Control ALU_Control(
 );
 
 HazardDetection HazardDetection(
-    .mux8_o             (),
-    .Flush_o            (),
+    .bubble_o           (),
     .IF_ID_rs_i         (),
     .IF_ID_rt_i         (),
     .ID_EX_rt_i         (),
-    .ID_EX_memread_i    ()
+    .ID_EX_MemRead_i    ()
 );
 
 endmodule
--- proj1/src/HazardDetection.v
+++ proj1/src/HazardDetection.v
@@ -1,20 +1,18 @@
 module HazardDetection
 (
-    mux8_o,  // bubble
-    Flush_o,
+    bubble_o,
     IF_ID_rs_i,
     IF_ID_rt_i,
     ID_EX_rt_i,
-    ID_EX_memread_i
+    ID_EX_MemRead_i
 );
 
 input   [4:0]   IF_ID_rs_i, IF_ID_rt_i, ID_EX_rt_i;
-input           ID_EX_memread_i;
-output          mux8_o, Flush_o;
+input           ID_EX_MemRead_i;
+output          bubble_o;
 
-assign {mux8_o, Flush_o} =
-    (ID_EX_memread_i && ((ID_EX_rs_i == ID_EX_rt_i) || (
-        (ID_EX_rs_i !== ID_EX_rt_i) && (ID_EX_rt_i == IF_ID_rt_i)
-    ))) ? 2'b00 : 2'b11;
+assign bubble_o = ID_EX_MemRead_i && (
+    (ID_EX_rs_i == ID_EX_rt_i) || (ID_EX_rt_i == IF_ID_rt_i)
+    );
 
 endmodule

76eec9dcda52ba688cee937deffed69c3ff567a6
Shao-Chuan Lee <lsc36x@gmail.com>
2014-12-28 23:21:10 +0800

    data memory modification

--- proj2/src/Data_Memory.v
+++ proj2/src/Data_Memory.v
@@ -1,26 +1,63 @@
 module Data_Memory
 (
-    read_data_o,
-    address_i,
-    write_data_i,
-    MemRead_i,
-    MemWrite_i
+    clk_i,
+    rst_i,
+    addr_i,
+    data_i,
+    enable_i,
+    write_i,
+    ack_o,
+    data_o
 );
 
-input   [31:0]  address_i, write_data_i;
-input           MemRead_i, MemWrite_i;
-output  [31:0]  read_data_o;
+input           clk_i, rst_i, enable_i, write_i;
+input   [31:0]  addr_i;
+input   [255:0] data_i;
+output          ack_o;
+output  [255:0] data_o;
 
-reg     [31:0]  read_data_o;
-reg     [7:0]   memory  [31:0];
+reg             ack_o;
+reg     [255:0] data_o;
+reg     [255:0] memory  [0:511];
 
-initial begin
-    read_data_o <= 0;
-end
+parameter       STATE_IDLE   = 0,
+                STATE_WAIT   = 1,
+                STATE_ACK    = 2,
+                STATE_FINISH = 3;
+reg     [2:0]   state;
+reg     [3:0]   count;
 
-always @(address_i or write_data_i or MemRead_i or MemWrite_i) begin
-    if (MemWrite_i) memory[address_i] = write_data_i;
-    if (MemRead_i) read_data_o = memory[address_i];
+always @(posedge clk_i or negedge rst_i) begin
+    if (~rst_i) begin
+        ack_o <= 0;
+        data_o <= 0;
+        state = STATE_IDLE;
+        count = 0;
+    end
+    else begin
+        case (state)
+            STATE_IDLE: begin
+                if (enable_i) begin
+                    count = count + 1;
+                    state = STATE_WAIT;
+                end
+            end
+            STATE_WAIT: begin
+                if (count == 10) state = STATE_ACK;
+                else count = count + 1;
+            end
+            STATE_ACK: begin
+                if (write_i) memory[addr_i >> 5] = data_i;
+                data_o = memory[addr_i >> 5];
+                ack_o = 1;
+                state = STATE_FINISH;
+            end
+            STATE_FINISH: begin
+                ack_o = 0;
+                state = STATE_IDLE;
+            end
+        endcase
+    end
 end
 
 endmodule

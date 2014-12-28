357da138a1e03057d9027c1d05855ea811001965
Shao-Chuan Lee <lsc36x@gmail.com>
2014-12-28 23:19:58 +0800

    Instruction memory size

--- proj2/src/Instruction_Memory.v
+++ proj2/src/Instruction_Memory.v
@@ -9,7 +9,7 @@ input   [31:0]      addr_i;
 output  [31:0]      instr_o;
 
 // Instruction memory
-reg     [31:0]     memory  [0:127];
+reg     [31:0]     memory  [0:511];
 
 assign  instr_o = memory[addr_i>>2];
 
